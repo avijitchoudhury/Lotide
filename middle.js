b7990e81e440f83d175d2b16c9e76d9226cb7702
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-18 00:01:32 +0000

    Implementing the middle array

---
+++ middle.js
@@ -0,0 +1,39 @@
+let eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false
+  }
+
+  for (var i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const assertArrayEquals = function (arrayOne, arrayTwo) {
+  let assertion = eqArrays(arrayOne, arrayTwo);
+  if (assertion) {
+    console.log(`Great job! It works!`)
+  } else {
+    console.log(`Unfortunately it is a fail!`)
+  }
+}
+
+const middle = function (array) {
+  let middleArray = Math.floor(array.length/2)
+  if(array.length <= 2){
+    return []
+  } else if (array.length % 2 === 1){
+    return [array[Math.floor(array.length/2)]]
+  } else if (array.length % 2 === 0) {
+    return [array[(array.length / 2) - 1], array[array.length / 2]]
+}
+}
+
+console.log(middle([]))
+console.log(middle([1, 2]))
+console.log(middle([1, 2, 3]))
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5]))
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
