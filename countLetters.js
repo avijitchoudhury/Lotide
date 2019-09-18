08fd174780f807117e20d9b3b19f853ace53b33c
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-18 19:05:46 +0000

    final function of countLetters

--- countLetters.js
+++ countLetters.js
@@ -6,6 +6,18 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const countLetters = function (allItems, itemsToCount) {
-  
+const countLetters = function (string) {
+  const results = {};
+  const stringWithoutSpace = string.replace(/\s+/g, '');  
+  for (const items of stringWithoutSpace) {
+      if (results[items]) {
+        results[items] += 1;
+      } else {
+        results[items] = 1;
+      }
+    }
+  return results;
 }
+
+console.log(countLetters("lighthouse in the house"));
+
