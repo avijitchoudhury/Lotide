1fed9385073c566ebec31352b0ae8bf326e51139
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-18 17:23:40 +0000

    created a countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,11 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Pass: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const countLetters = function (allItems, itemsToCount) {
+  
+}
