f3a6c608d2b504e22b955633cc100528f6d58f45
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-19 22:35:09 +0000

    Implemented the findKey function

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Pass: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const findKey = function (obj, callback) {
+  for (const item in obj) {
+    if (callback(obj[item])) {
+      return item
+    }
+  }
+}
+
+
+const name = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(name)
+
+
