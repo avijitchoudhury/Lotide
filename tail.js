83beead4002679b52170802a5e732891efd1bcff
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-23 17:00:57 +0000

    created test assertion for tail function

--- tail.js
+++ tail.js
@@ -1,18 +1,17 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Pass: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅Assertion Pass: ${[actual]} === ${[expected]}`);
+//   } else {
+//     console.log(`❌❌❌Assertion Failed: ${[actual]} !== ${[expected]}`);
+//   }
+// };
+
+//Test code
+const assertEqual = require('./assertEqual')
 
 const tail = function (objectOne) {
   let mainTail = objectOne.slice(1);
   return mainTail
 }
 
-const result = tail(["Hello", "Lighthouse", "Labs", "Avijit", "School"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(result[1], "Avijit")
+module.exports = tail;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,11 +2,15 @@
 const assertEqual = require('../assertEqual')
 const head = require('../head')
 
+
+
+//Test for assertEqual.js
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Toronto", "Toronto");
 assertEqual("Candy", "candy");
 
-
+//Test for head.js
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
